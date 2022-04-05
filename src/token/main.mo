99c1007e69c07c20d90a2bd1ca9849c532e35467
Raúl Osuna <raulosuna@gmail.com>
2022-04-05 14:42:37 +0200

    Faucet functionality

--- src/token/main.mo
+++ src/token/main.mo
@@ -1,5 +1,6 @@
 import Principal "mo:base/Principal";
 import HashMap "mo:base/HashMap";
+import Debug "mo:base/Debug";
 
 actor Token {
     var owner : Principal = Principal.fromText("fvc3b-dn5ks-2kftz-3xwzg-pdiof-mk7zf-qb2vr-mxnp5-nnq5v-u4fob-nqe");
@@ -23,5 +24,16 @@ actor Token {
 
     public query func getSymbol() : async Text {
         return symbol;
+    };
+
+    public shared(msg) func payOut() : async Text {
+        //Debug.print(debug_show(msg.caller));
+        if (balances.get(msg.caller) == null) {
+            let amount = 10000;
+            balances.put(msg.caller, amount);
+            return "Success";
+        } else {
+            return "Already claimed";
+        }
     };
 };
--- src/token_assets/src/components/Faucet.jsx
+++ src/token_assets/src/components/Faucet.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import { token } from "../../../declarations/token";
 
 function Faucet() {
 
-  async function handleClick(event) {
+  const [isDisabled, setDisable] = useState(false);
+  const [buttonText, setText] = useState("Gimme gimme");
 
+  async function handleClick(event) {
+    setDisable(true);
+    const result = await token.payOut();
+    setText(result);
+    // setDisable(false);
   }
 
   return (
@@ -14,10 +21,10 @@ function Faucet() {
         </span>
         Faucet
       </h2>
-      <label>Get your free DAngela tokens here! Claim 10,000 DANG coins to your account.</label>
+      <label>Get your free DAngela tokens here! Claim 10,000 DANG tokens to your account.</label>
       <p className="trade-buttons">
-        <button id="btn-payout" onClick={handleClick}>
-          Gimme gimme
+        <button id="btn-payout" onClick={handleClick} disabled={isDisabled}>
+          {buttonText}
         </button>
       </p>
     </div>
